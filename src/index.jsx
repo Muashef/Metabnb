806931a53bf2d325a72fbbe496e099cf42f2b939
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-11-11 18:21:35 +0100

    setup routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from './pages/Home';
+import PlacesToStay from './pages/PlacesToStay';
+
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/places-to-stay", element: <PlacesToStay /> },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <main>
+      <RouterProvider router={router} />
+    </main>
   </React.StrictMode>
 );
 
---
+++ src/pages/PlacesToStay.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function PlacesToStay() {
+  return (
+    <div>
+      <h1>Places to Stay</h1>
+    </div>
+  );
+}
