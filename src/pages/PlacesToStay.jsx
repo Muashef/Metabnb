ec81063fe3f9e1a8f430159ca042a9e9ae221957
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-11-13 23:16:09 +0100

    add footer and card grid to places to stay

--- src/pages/PlacesToStay.jsx
+++ src/pages/PlacesToStay.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import CardGrid from "../components/CardGrid";
+import Footer from "../components/Footer";
 
 export default function PlacesToStay() {
   return (
     <div>
       <h1>Places to Stay</h1>
+      <section className="hero sm:px-[100px] px-4 sm:pt-10 pt-5 sm:pb-14 pb-8">
+        <CardGrid size={16} />
+      </section>
+      <section>
+        <Footer />
+      </section>
     </div>
   );
 }
